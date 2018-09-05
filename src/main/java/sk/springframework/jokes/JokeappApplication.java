f76405ac0551723f71a28c910866fff52407fc3d
Shivaranjini <s.arumugaperumal@walmartlabs.com>
2018-09-05 15:13:36 +0530

    Using annotation and xml based configuration

--- src/main/java/sk/springframework/jokes/JokeappApplication.java
+++ src/main/java/sk/springframework/jokes/JokeappApplication.java
@@ -2,8 +2,12 @@ package sk.springframework.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"sk.springframework.jokes"})
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/sk/springframework/jokes/config/ChuckConfiguration.java
@@ -0,0 +1,12 @@
+package sk.springframework.jokes.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+//@Configuration
+public class ChuckConfiguration {
+
+    //@Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
